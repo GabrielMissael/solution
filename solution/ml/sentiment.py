ebc21b898dce968877b87b749667bf799b6425d1
GabrielMissael <missael_gabo@hotmail.com>
2021-10-24 07:20:43 -0500

    Updated sentiment analysis script

--- solution/ml/sentiment.py
+++ solution/ml/sentiment.py
@@ -8,15 +8,13 @@ def deEmojify(text):
     return emoji.get_emoji_regexp().sub(r'', text)
 
 def AddSentimentAnalysis(tweet_df_route:str):
-    df = pd.DataFrame(tweet_df_route)
+    df = pd.read_pickle(tweet_df_route)
 
     df = df[df['labels'] != -1]
 
-    tweets_count = df['labels'].value_counts()
-    top_labels = [x[0] for x in tweets_count.items() if x[1] > 50]
+    top_labels = df['labels'].value_counts()[:3].keys().to_list()
 
     df = df[df['labels'].isin(top_labels)]
-    df.head(2)
 
     tweets = df['text'].to_list()
 
@@ -28,8 +26,10 @@ def AddSentimentAnalysis(tweet_df_route:str):
     sentiment_list=[]
     for i, tweet in enumerate(clean_tweets):
         tweet = deEmojify(tweet)
+        tweet = translator.translate(tweet, src='es', dest='en').text
         sentiment_dict = sentiment.polarity_scores(tweet)
         sentiment_list.append(sentiment_dict['compound'])
+
     df['Sentiment'] = sentiment_list
 
     return df
