b540d8b3e54be67858635fa3ede96344279bf158
GabrielMissael <missael_gabo@hotmail.com>
2021-10-23 23:27:14 -0500

    Added clustering module

--- solution/ml/clustering.py
+++ solution/ml/clustering.py
@@ -12,8 +12,7 @@ import hdbscan  # Clustering
 import time
 
 
-def cluster(file, plot=False, verbose=False, impath='scatter.html',
-            n_neighbors=15, min_dist=0.0,
+def cluster(file, verbose=False, n_neighbors=15, min_dist=0.0,
             min_cluster_size=10, min_samples = 1,):
 
     """Create clusters of tweets with similar meaning.
@@ -97,34 +96,6 @@ def cluster(file, plot=False, verbose=False, impath='scatter.html',
     # Save plot coordinates
     df['x'] = umap_plot.T[0]
     df['y'] = umap_plot.T[1]
-
-    if plot == True:
-
-        df_plot = df.copy()
-
-        # Labels to stirngs
-        df_plot['labels'] = df_plot['labels'].astype('str')
-
-        # For dot size
-        df_plot['log_retweet'] = df_plot['retweet_count'].apply(
-            lambda x: np.log(x+2))
-
-        # Remove unclustered tweets
-        df_plot = df_plot[df_plot['labels'] != '-1']
-
-        # If too many clusters, only plot the bigger ones.
-        if len(set(df_plot['labels'])) > 10:
-            top = list(df_plot['labels'].value_counts()[:10].index)
-            mapit = [x in top for x in df_plot['labels']]
-            df_plot = df_plot[mapit]
-
-        # Create scatter visualization
-        fig = px.scatter(df_plot, x="x", y="y", color='labels',
-                         size='log_retweet', hover_name='text',
-                         title='Clusters of tweets with similar meaning')
-
-        # Save dataviz
-        plotly.offline.plot(fig, filename=impath)
 
     if verbose:
         print(time.time() - start_time, " seconds of execution ⌚")
